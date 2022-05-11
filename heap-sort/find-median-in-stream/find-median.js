c268d2fc3302d816fc6e9ac566546f2d7ec6a4a7
Lim <siewleng.lim@dxc.com>
2022-05-11 10:59:32 +0800

    find median in stram

---
+++ heap-sort/find-median-in-stream/find-median.js
@@ -0,0 +1,155 @@
+// { Driver Code Starts
+//Initial Template for javascript
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Function to print an array */
+function printArray(arr, size) {
+  let i;
+  let s = "";
+  for (i = 0; i < size; i++) {
+    if(arr[i] == -0)
+      arr[i] = 0;
+    s += arr[i] + " ";
+  }
+  console.log(s);
+}
+
+function main() {
+  let t = parseInt(readLine());
+  let i = 0;
+  for (; i < t; i++) {
+    let n = parseInt(readLine());
+    let obj = new Solution();
+    for(let j = 0;j<n;j++){
+      let x = parseInt(readLine());
+      obj.insertHeap(x);
+      console.log(Math.floor(obj.getMedian()));
+    }
+  }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ *
+ *insertHeap
+ * @param {number} x
+ *
+ *getMedian
+ * @return {float}
+ */
+class Solution
+{
+    // Function to insert heap
+    array = [];
+
+
+    swapArray(a, b, arr) {
+      const temp = arr[a];
+      arr[a] = arr[b];
+      arr[b] = temp;
+    }
+
+    heapifyFromBottomToTop (index, arr) {
+      // index = index of array + 1;
+      if (index > 1) {
+        const parentIndex = Math.floor(index / 2);
+        const parentValue = arr[parentIndex - 1];
+        const currentValue = arr[index - 1];
+        if (currentValue > parentValue) {
+          this.swapArray(parentIndex - 1, index - 1, arr);
+          this.heapifyFromBottomToTop(parentIndex, arr);
+        }
+      }
+    }
+
+    heapifyFromTopToBottom (index, stopIndex, arr) {
+      // index = index of array + 1;
+      const leftChildIndex = 2 * index;
+      const rigthChildIndex = 2 * index + 1;
+      let largestValueIndex = index;
+      let largestValue = arr[index - 1];
+      if (leftChildIndex < stopIndex + 1 &&
+        arr[leftChildIndex - 1] > largestValue) {
+        largestValue = arr[leftChildIndex - 1];
+        index = leftChildIndex;
+      }
+      if (rigthChildIndex < stopIndex + 1 &&
+        arr[rigthChildIndex - 1] > largestValue) {
+        largestValue = arr[rigthChildIndex - 1];
+        index = rigthChildIndex;
+      }
+      if (largestValueIndex !== index) {
+        this.swapArray(index - 1, largestValueIndex - 1, arr);
+        this.heapifyFromTopToBottom(largestValueIndex, stopIndex, arr);
+      }
+    }
+
+    insertHeap(x)
+    {
+        // add your code here
+        this.array.push(x);
+        this.balanceHeaps();
+    }
+    
+     // Function to balance Heaps
+    balanceHeaps()
+    {
+       // add your code here
+       this.heapifyFromBottomToTop(this.array.length, this.array);
+    }
+
+    getMedianFromArray(arr) {
+      if (arr.length % 2 === 1) {
+        const index = (arr.length + 1 ) / 2;
+        return arr[index - 1];
+      }
+      const index = arr.length / 2;
+      return ((arr[index - 1] + arr[index]) / 2);
+    }
+
+    // function to getMedian
+    getMedian()
+    {
+        // add your code here
+        let n = this.array.length;
+        const array = [];
+        this.array.forEach((item) => {
+          array.push(item);
+        });
+        while (n >= 2) {
+          this.swapArray(0, n - 1, array);
+          n = n - 1;
+          this.heapifyFromTopToBottom(1, n, array);
+        }
+        return this.getMedianFromArray(array);
+    } 
+}
